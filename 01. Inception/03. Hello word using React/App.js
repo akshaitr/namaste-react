302ce0dd8393e4de4673267f349abbf53ac936da
Akshai T R <akshaitr1@gmail.com>
2024-07-23 06:11:04 +0530

    Code cleanup

--- 01. Inception/03. Hello word using React/App.js
+++ 01. Inception/03. Hello word using React/App.js
@@ -2,19 +2,26 @@
  * Create an element using React
  */
 
-const heading = React.createElement("h1", {}, "Hello World!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {}, "Hello World!");
 root.render(heading);
 
 /**
  * Create nested elements using React
  */
-const listItem1 = React.createElement("li", { key: "list-item-1" }, "Item 1");
-const listItem2 = React.createElement("li", { key: "list-item-2" }, "Item 1");
-const listItem3 = React.createElement("li", { key: "list-item-3" }, "Item 1");
-const listItem4 = React.createElement("li", { key: "list-item-4" }, "Item 1");
-const listItem5 = React.createElement("li", { key: "list-item-5" }, "Item 1");
+const nestedRoot = ReactDOM.createRoot(document.getElementById("nestedRoot"));
 
+const listHeading = React.createElement(
+  "h3",
+  { key: "listHeading" },
+  "Nested React Elements"
+);
+
+const listItem1 = React.createElement("li", { key: "list-item-1" }, "Item 1");
+const listItem2 = React.createElement("li", { key: "list-item-2" }, "Item 2");
+const listItem3 = React.createElement("li", { key: "list-item-3" }, "Item 3");
+const listItem4 = React.createElement("li", { key: "list-item-4" }, "Item 4");
+const listItem5 = React.createElement("li", { key: "list-item-5" }, "Item 5");
 const unorderedList = React.createElement("ul", { key: "list" }, [
   listItem1,
   listItem2,
@@ -23,17 +30,9 @@ const unorderedList = React.createElement("ul", { key: "list" }, [
   listItem5,
 ]);
 
-const listHeading = React.createElement(
-  "h3",
-  { key: "listHeading" },
-  "Nested React Elements"
-);
-
 const listContainer = React.createElement("div", {}, [
   listHeading,
   unorderedList,
 ]);
-
-const nestedRoot = ReactDOM.createRoot(document.getElementById("nestedRoot"));
 
 nestedRoot.render(listContainer);
--- 02. Igniting our app/App.js
+++ 02. Igniting our app/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello World!");
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const heading = React.createElement("h1", {}, "Hello World!");
 
 root.render(heading);
